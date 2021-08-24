3e67aabcb0364e0f48b1a005ffe13aa238a4eaf1
Sravan voleti <sravankumar@codeaurora.org>
2021-08-24 22:45:02 +0530

    Bluetooth: Update Bluetooth permissions for internal APIS. (2/4)
    This adds attribution source as a parameter to Value added APIS.
    
    Update Bluetooth API annotations.
    
    CRs-Fixed: 3044065
    Change-Id: Icf9c0242f928ce2f26f66350ddb3937fda35220d

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2046,8 +2046,9 @@ public final class BluetoothAdapter {
 
         return false;
     }
+
     /** @hide */
-    @RequiresPermission(Manifest.permission.BLUETOOTH)
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH)
     public boolean isBroadcastActive() {
         try {
             mServiceLock.readLock().lock();
